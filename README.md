4ace9933484b738f16f6e9c58e380e6e167ce159
UriKatsirPrivate <46541339+UriKatsirPrivate@users.noreply.github.com>
2021-01-29 22:31:48 +0200

    initial

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # workflow-workshop
  GCP workflows workshop for Machine Images
+
+ https://cloud.google.com/compute/docs/reference/rest/beta/machineImages
+
+ 
--- delete_machine_image.py
+++ delete_machine_image.py
@@ -1,4 +1,6 @@
 # from google_auth_oauthlib.flow import InstalledAppFlow
+import googleapiclient.discovery
+from google.oauth2 import service_account
 from googleapiclient.discovery import build
 import json
 import sys
@@ -6,12 +8,7 @@ import datetime
 from datetime import datetime, timedelta
 import pytz
 
-utc=pytz.UTC
-
-
-
-from google.oauth2 import service_account
-import googleapiclient.discovery
+utc = pytz.UTC
 
 #  Authenticate using Service Account
 SCOPES = ['https://www.googleapis.com/auth/cloud-platform']
@@ -29,22 +26,27 @@ gce_service = build('compute', 'beta', credentials=credentials)
 # -------------------------------------------------------------
 
 # def Insert_Machine_Image(request):
+
+
 def Delete_Machine_Image():
     # request_json = request.get_json()
     # project = request_json['project']
     project = 'uri-test'
-    days_to_go_back=5
-    Machine_Images = gce_service.machineImages().list(project=project).execute()
+    # days_to_go_back=5
+    days_to_go_back = int(request_json['days_to_go_back'])
+    Machine_Images = request_json['items']
+    # Machine_Images = gce_service.machineImages().list(project=project).execute()
     # instances = request_json['items']
     for instance in Machine_Images['items']:
-        name = instance['name']
-        CreationTimestamp = datetime.fromisoformat(instance['creationTimestamp'])
-        DeletionDate = utc.localize(datetime.today() - timedelta(days=days_to_go_back))
+        name = Machine_Image['name']
+        CreationTimestamp = datetime.fromisoformat(
+            Machine_Image['creationTimestamp'])
+        DeletionDate = utc.localize(
+            datetime.today() - timedelta(days=days_to_go_back))
 
-        if CreationTimestamp<DeletionDate:
-            gce_service.machineImages().delete(project=project,machineImage=name).execute()
+        if CreationTimestamp < DeletionDate:
+            gce_service.machineImages().delete(project=project, machineImage=name).execute()
     # ddd = ''
 
-    
 
-Delete_Machine_Image()
+# Delete_Machine_Image()
