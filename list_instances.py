3b6a1cf2ba36a3097126382b98085d05816c089d
UriKatsirPrivate <46541339+UriKatsirPrivate@users.noreply.github.com>
2021-01-29 13:59:14 +0200

    Works in Cloud Functions

--- list_instances.py
+++ list_instances.py
@@ -1,22 +1,12 @@
-from google_auth_oauthlib.flow import InstalledAppFlow
+# from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 import json
 import sys
-import create_machine_image
+# import create_machine_image
 
 
 from google.oauth2 import service_account
 import googleapiclient.discovery
-
-
-# Authenticate from local machine
-# flow = InstalledAppFlow.from_client_secrets_file(
-#     'security/client_secret_960394617171.json',
-#     scopes=['https://www.googleapis.com/auth/cloud-platform'])
-
-# credentials = flow.run_local_server()
-# credentials = flow.run_console()
-# ===================================================================
 
 #  Authenticate using Service Account
 SCOPES = ['https://www.googleapis.com/auth/cloud-platform']
@@ -25,26 +15,28 @@ credentials = service_account.Credentials.from_service_account_file(
     SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 # ===================================================================
 
-
+# Local testing
 gce_service = build('compute', 'v1', credentials=credentials)
+# -------------------------------------------------------------
 
-project_name = 'uri-test'
-zone = 'us-east1-b'
+# Run on GCP
+# gce_service = build('compute', 'v1')
+# -------------------------------------------------------------
 
-
-def list_instances(project_name, zone):
+def list_instances(request):
+    request_json = request.get_json()
+    project = request_json['project']
+    zone = request_json['zone']
     instances = gce_service.instances().list(
-        project=project_name, zone=zone).execute()
-    for instance in instances['items']:
-        name = instance['name']
-        name = (name[:40]) if len(name) > 40 else name
-        selfLink = instance['selfLink']
+        project=project, zone=zone).execute()
+    # for instance in instances['items']:
+    #     name = instance['name']
+    #     name = (name[:40]) if len(name) > 40 else name
+    #     selfLink = instance['selfLink']
     
-    create_machine_image.Insert_Machine_Image(project_name,instances)
-        
-
+    # create_machine_image.Insert_Machine_Image(project_name,instances)
         
     return instances
 
-
-list_instances(project_name, zone)
+input1 = {"project": 5, "zone": "us-east1-b"}
+list_instances(input1)
