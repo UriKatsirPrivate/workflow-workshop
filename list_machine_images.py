c8a39819d5f7302c4093de72af0bdad6ae6ea31a
UriKatsirPrivate <46541339+UriKatsirPrivate@users.noreply.github.com>
2021-01-29 22:31:15 +0200

    rename

--- list_machine_images.py
+++ list_machine_images.py
@@ -23,21 +23,16 @@ gce_service = build('compute', 'beta', credentials=credentials)
 # gce_service = build('compute', 'beta')
 # -------------------------------------------------------------
 
-# config = {
-#     "name": datetime.datetime.now().strftime(("%Y-%m-%d-%H-%M-%S")),
-#     "sourceInstance": "https://www.googleapis.com/compute/v1/projects/uri-test/zones/us-east1-b/instances/vpn-from-aws-1"
-# }
-
 
 # def Insert_Machine_Image(request):
-def List_Machine_Image():
+def List_Machine_Images():
     # request_json = request.get_json()
     # project = request_json['project']
     project = 'uri-test'
     # instances = request_json['items']
     Machine_Images = gce_service.machineImages().list(project=project).execute()
-    ddd = ''
 
     return Machine_Images
 
-List_Machine_Image()
+
+List_Machine_Images()
