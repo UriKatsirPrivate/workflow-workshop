af88b059fb6376dc4ae4d677f9e28d7f374847cf
UriKatsirPrivate <46541339+UriKatsirPrivate@users.noreply.github.com>
2021-02-02 19:02:33 +0200

    Code cleanup

--- delete_machine_image.py
+++ delete_machine_image.py
@@ -31,18 +31,17 @@ gce_service = build('compute', 'beta', credentials=credentials)
 def Delete_Machine_Image():
     # request_json = request.get_json()
     # project = request_json['project']
+    # delete_images_older_than_days = 5
     project = 'uri-test'
-    days_to_go_back=1
+    days_to_go_back = 3
     # days_to_go_back = int(request_json['days_to_go_back'])
     # Machine_Images = request_json['items']
     Machine_Images = gce_service.machineImages().list(project=project).execute()
     # Machine_Image = request_json['items']
     for Machine_Image in Machine_Images['items']:
         name = Machine_Image['name']
-        CreationTimestamp = datetime.fromisoformat(
-            Machine_Image['creationTimestamp'])
-        DeletionDate = utc.localize(
-            datetime.today() - timedelta(days=days_to_go_back))
+        CreationTimestamp = datetime.fromisoformat(Machine_Image['creationTimestamp'])
+        DeletionDate = utc.localize(datetime.today() - timedelta(days=days_to_go_back))
 
         if CreationTimestamp < DeletionDate:
             gce_service.machineImages().delete(project=project, machineImage=name).execute()
