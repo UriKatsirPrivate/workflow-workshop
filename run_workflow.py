8ab0ad10eecddafe6791908ce4bdea6da00f96ee
UriKatsirPrivate <46541339+UriKatsirPrivate@users.noreply.github.com>
2021-02-10 23:40:48 +0200

    Cleanup

--- run_workflow.py
+++ run_workflow.py
@@ -1,4 +1,3 @@
-# https://cloud.google.com/workflows/docs/reference/executions/rest/v1/projects.locations.workflows.executions/create
 from googleapiclient.discovery import build
 import json
 import sys
@@ -6,35 +5,16 @@ import sys
 from google.oauth2 import service_account
 import googleapiclient.discovery
 
-#  Authenticate using Service Account
-SCOPES = ['https://www.googleapis.com/auth/cloud-platform']
-SERVICE_ACCOUNT_FILE = 'security/uri-test-38aacab75c2a.json'
-credentials = service_account.Credentials.from_service_account_file(
-    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-# ===================================================================
 
-# Local testing
-workflow_service = build('workflowexecutions', 'v1', credentials=credentials)
-# -------------------------------------------------------------
-
-# Run on GCP
-# workflow_service = build('workflowexecutions', 'v1')
-# -------------------------------------------------------------
+workflow_service = build('workflowexecutions', 'v1')
 
 
 def run_workflow(request):
-    # request_json = request.get_json()
-    # parent = request_json['parent']
-    parent = "projects/uri-test/locations/us-central1/workflows/CreateMachineImage"
-    # body = request_json['body']
-    body = {"argument": '{"project": "uri-test","zone": "us-east1-b"}'}
-    workflow = workflow_service.projects().locations().workflows().executions().create(
-      parent= parent, body= body).execute()
+    request_json = request.get_json()
+    parent = request_json['parent']
+    body = request_json['body']
 
-    # The code below will cause the workflow to fail because no body is provided
-    # workflow = workflow_service.projects().locations().workflows().executions().create(
-    #   parent= parent).execute()  
+    workflow = workflow_service.projects().locations().workflows().executions().create(
+        parent=parent, body=body).execute()
 
     return workflow
-
-run_workflow('')
