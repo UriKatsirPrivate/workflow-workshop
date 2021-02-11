b2d8aae58faf0458a70555f6e4c12028de2becb1
UriKatsirPrivate <46541339+UriKatsirPrivate@users.noreply.github.com>
2021-02-11 20:40:38 +0200

    Changed days_to_go_back to days_to_keep_images

--- delete_machine_image.py
+++ delete_machine_image.py
@@ -16,14 +16,14 @@ gce_service = build('compute', 'beta')
 def Delete_Machine_Image(request):
     request_json = request.get_json()
     project = request_json['project']
-    days_to_go_back = int(request_json['days_to_go_back'])
+    days_to_keep_images = int(request_json['days_to_keep_images'])
     Machine_Images = request_json['items']
     for Machine_Image in Machine_Images:
         name = Machine_Image['name']
         CreationTimestamp = datetime.fromisoformat(
             Machine_Image['creationTimestamp'])
         DeletionDate = utc.localize(
-            datetime.today() - timedelta(days=days_to_go_back))
+            datetime.today() - timedelta(days=days_to_keep_images))
 
         if CreationTimestamp < DeletionDate:
             gce_service.machineImages().delete(project=project, machineImage=name).execute()
